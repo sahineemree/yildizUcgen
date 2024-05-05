6238872d856f4186224efd80a0970f67e5705659
Emre Şahin <147410835+sahineemree@users.noreply.github.com>
2024-05-05 16:07:26 +0300

    Add files via upload

---
+++ yildizUcgen.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class yildizUcgen {
+    public static void main(String[] args) {
+
+        int n;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Basamak sayısını giriniz : ");
+        n = input.nextInt();
+
+        for (int i = 1 ; i<=n ; i++) {
+
+            for (int k = 1; k <= (n - i); k++)
+
+                System.out.print(" ");
+
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
